9e5d20066917223ed2eedd87657f89cd99c9114e
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-02-09 20:07:07 +0800

    Create replace.js

---
+++ replace.js
@@ -0,0 +1,49 @@
+const PI = Math.PI;
+
+var r = {};
+
+r.replace = function(answers) {
+    answers = answers.replace(/美轮美./g, "美轮美奂");
+    answers = answers.replace(/决穿/g, "诀窍");
+    answers = answers.replace(/浙临/g, "濒临");
+    answers = answers.replace(/不落../g, "不落窠臼");
+    answers = answers.replace(/.目结舌/g, "膛目结舌");
+    answers = answers.replace(/泉水../g, "泉水淙淙");
+    answers = answers.replace(/饮.止渴/g, "饮鸠止渴");
+    answers = answers.replace(/趋之若./g, "趋之若鹜");
+    answers = answers.replace(/一.而就/g, "一蹴而就");
+    answers = answers.replace(/刚.自用/g, "刚愎自用");
+    answers = answers.replace(/风驰电./g, "风驰电掣");
+    answers = answers.replace(/不.而走/g, "不胫而走");
+    answers = answers.replace(/.声叹气/g, "唉声叹气");
+    answers = answers.replace(/.而走险/g, "铤而走险");
+    answers = answers.replace(/底护/g, "庇护");
+    answers = answers.replace(/蓓./g, "蓓蕾");
+    answers = answers.replace(/抵悟/g, "抵牾");
+    answers = answers.replace(/情懒/g, "慵懒");
+    answers = answers.replace(/差道/g, "差遣");
+    answers = answers.replace(/泽炼/g, "淬炼");
+    answers = answers.replace(/博奔/g, "博弈");
+    answers = answers.replace(/相形见./g, "相形见绌");
+    answers = answers.replace(/对.公堂/g, "对簿公堂");
+    answers = answers.replace(/疼李/g, "痉挛");
+    answers = answers.replace(/痉李/g, "痉挛");
+    answers = answers.replace(/..人口/g, "脍炙人口");
+    answers = answers.replace(/.意安为/g, "恣意妄为");
+    answers = answers.replace(/凌合/g, "凑合");
+    answers = answers.replace(/神抵/g, "神祗");
+    answers = answers.replace(/叫苦不./g, "叫苦不迭");
+    answers = answers.replace(/草.人命/g, "草菅人命");
+    answers = answers.replace(/鞭./g, "鞭笞");
+    answers = answers.replace(/发物/g, "发轫");
+    answers = answers.replace(/..充数/g, "滥芋充数");
+    answers = answers.replace(/水蒸气/g, "水蒸气 水蒸汽");
+    answers = answers.replace(/..置疑/g, "毋庸置疑");
+    answers = answers.replace(/..不振/g, "萎靡不振");
+    answers = answers.replace(/瓜熟.落/g, "瓜熟蒂落");
+    answers = answers.replace(/虎视../g, "虎视眈眈");
+    answers = answers.replace(/进裂/g, "崩裂");
+    return answers
+};
+
+module.exports = r;
