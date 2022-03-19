0c298f681da3270e23ac94875aa90da489a2a4e1
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-03-19 17:51:02 +0800

    Update replace.js

--- replace.js
+++ replace.js
@@ -3,10 +3,9 @@ const PI = Math.PI;
 var r = {};
 
 r.replace = function(answers) {
-    answers = answers.replace(/+/g, "十");
+    answers = answers.replace(/\+/g, "十");
     answers = answers.replace(/李诚/g, "李诫");
     answers = answers.replace(/蹄红/g, "蹿红");
-    answers = answers.replace(/+/g, "十");
     return answers
 };
 
