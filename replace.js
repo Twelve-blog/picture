e2e3948cc4c95d0694fd98b0758ed55861b78f0c
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-03-21 00:52:17 +0800

    Update replace.js

--- replace.js
+++ replace.js
@@ -3,6 +3,7 @@ const PI = Math.PI;
 var r = {};
 
 r.replace = function(answers) {
+    if(answers.indexOf('泰')!=-1&&answers.indexOf('樱')!=-1) answers = answers.replace(/泰/g, "菽");
     if(answers.indexOf('缘')!=-1&&answers.indexOf('舜')!=-1) answers = answers.replace(/缘/g, "鲧");
     answers = answers.replace(/.阳湖/g, "潘阳湖");
     answers = answers.replace(/饮鸽止渴/g, "饮鸩止渴");
