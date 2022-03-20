fc4c8e0a44c0689c8279f3c6132312afb1bc64b8
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-03-21 00:50:44 +0800

    Update replace.js

--- replace.js
+++ replace.js
@@ -3,6 +3,7 @@ const PI = Math.PI;
 var r = {};
 
 r.replace = function(answers) {
+    if(answers.indexOf('缘')!=-1&&answers.indexOf('舜')!=-1) answers = answers.replace(/缘/g, "鲧");
     answers = answers.replace(/.阳湖/g, "潘阳湖");
     answers = answers.replace(/饮鸽止渴/g, "饮鸩止渴");
     answers = answers.replace(/何族/g, "侗族");
