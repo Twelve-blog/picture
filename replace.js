e70d5a94494df07e11697738f3c8ee671df8dbce
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-03-21 15:36:01 +0800

    Update replace.js

--- replace.js
+++ replace.js
@@ -6,7 +6,6 @@ r.replace = function(answers) {
     if(answers.indexOf('戈')!=-1&&answers.indexOf('矛')!=-1) answers = answers.replace(/载/g, "戟");
     if(answers.indexOf('泰')!=-1&&answers.indexOf('樱')!=-1) answers = answers.replace(/泰/g, "菽");
     if(answers.indexOf('缘')!=-1&&answers.indexOf('舜')!=-1) answers = answers.replace(/缘/g, "鲧");
-    answers = answers.replace(/准备活动放松活动/g, "");
     answers = answers.replace(/快密/g, "诀窍");
     answers = answers.replace(/决密/g, "诀窍");
     answers = answers.replace(/令媛/g, "令嫒");
