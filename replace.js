fb20b91111ab6e32bcefe700088e9ac9d4061d3a
十二 <65481267+Twelve-blog@users.noreply.github.com>
2022-03-23 10:19:23 +0800

    Update replace.js

--- replace.js
+++ replace.js
@@ -7,6 +7,7 @@ r.replace = function(answers) {
     if(answers.indexOf('戈')!=-1&&answers.indexOf('矛')!=-1) answers = answers.replace(/载/g, "戟");
     if(answers.indexOf('泰')!=-1&&answers.indexOf('樱')!=-1) answers = answers.replace(/泰/g, "菽");
     if(answers.indexOf('缘')!=-1&&answers.indexOf('舜')!=-1) answers = answers.replace(/缘/g, "鲧");
+    if(answers.indexOf('放松活动')!=-1&&answers.indexOf('基本活动')!=-1) answers = answers.replace(/一/g, "");
     answers = answers.replace(/鱼鲤/g, "鱼鳔");
     answers = answers.replace(/沉缅/g, "沉湎");
     answers = answers.replace(/表秀/g, "裴秀");
